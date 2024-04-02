d459c739ce2c0bee8e816b95df65fe36add8b613
pjaewon <121800564+pjaewon@users.noreply.github.com>
2024-04-02 15:05:58 +0900

    uapp.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def search_again():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port =5000, debug=True)
 # if __name__ == '__main__':
 #     from waitress import serve
 #     serve(app, host="0.0.0.0", port=7396)
